daff4590550b716046c431ad0c2f02d2b26a137d
s-nakai <s-nakai@nisso.co.jp>
2021-03-22 15:03:21 +0900

    change title #1

--- src/components/templates/Main.js
+++ src/components/templates/Main.js
@@ -6,7 +6,7 @@ import CloseButton from "../atoms/CloseButton";
 const Main = () => {
   return (
     <main className="l-main">
-      <h1>登録会会場検索</h1>
+      <h1>登録会場検索</h1>
       <SearchBox />
       <Venues />
       <CloseButton />
